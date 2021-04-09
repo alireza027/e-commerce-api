bd7bba0a616ec22286a5a0c79eaf5d0b1d042738
alireza27 <hozhabrialireza27@gmail.com>
2021-04-10 00:33:26 +0430

    add models files

--- app.ts
+++ app.ts
@@ -1,18 +1,40 @@
 // add dependencies
-import express, { Application, Request, Response } from "express";
-// import bodyParser from "body-parser";
-// import mongoose from 'mongoose';
+import express, { Application } from "express";
+import * as mongoose from "mongoose";
+import { json, urlencoded } from "body-parser";
 import "dotenv/config";
+
+// add routes
 
 // create instance
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response) => {
-      console.log("Alireza");
-      res.send(process.env.PORT);
-});
+// use dependencies
+app.use(express.static(__dirname + "/public"));
+app.use(
+    json({
+        inflate: true,
+        limit: "100kb",
+        type: "application/*",
+        verify: () => {
+            return true;
+        }
+    })
+);
+app.use(urlencoded({ extended: false }));
+
+// use routes
+
+// connect to mongoose
+mongoose
+    .connect(process.env.MONGO_ADDRESS || "mongodb://localhost:27017/shop", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then()
+    .catch();
 
 // use experss
 app.listen(5001, () => {
-      console.log("Listening On 5001 PORT");
+    console.log(`Listening On ${process.env.PORT} PORT`);
 });
